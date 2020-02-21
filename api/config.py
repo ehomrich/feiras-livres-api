96055cf7683930dc1a72dd3492402e9d88ae79e2
Émerson P. Homrich <12125514+ehomrich@users.noreply.github.com>
2020-02-20 21:48:11 -0300

    Enable SQL Alchemy query echo on debug/development

--- api/config.py
+++ api/config.py
@@ -9,3 +9,4 @@ class AppConfig:
     SECRET_KEY = config('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/dev.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = config('FLASK_ENV', default='development') == 'development'
